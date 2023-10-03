27cd22d3abf406a7be037e4f808c01198b49b067
teshdev <gebrewold2012@gmail.com>
2023-10-03 14:08:52 -0700

    config all routes

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,8 @@ use App\Http\Controllers\Api\ContactController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Route::get('contacts',)
 Route::get('contacts', [ContactController::class, 'index']);
 Route::post('contacts', [ContactController::class, 'store']);
-Route::get('contacts/{contact}', [ContactController::class, 'edit']);
-Route::put('contacts/{contact}', [ContactController::class, 'update']);
-Route::delete('contacts/{contact}', [ContactController::class, 'destroy']);
+Route::get('contacts/{id}', [ContactController::class, 'show']);
+Route::put('contacts/{id}', [ContactController::class, 'update']);
+Route::delete('contacts/{id}', [ContactController::class, 'destroy']);
