81ef089664d07e7c8032f30c71d5f5c770a01210
teshdev <gebrewold2012@gmail.com>
2023-10-03 15:14:48 -0700

    vue added & vue-loader for ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
+
+import Contacts from './component/Contacts'
+
+const app = createApp({})
+
+app.component('contacts', Contacts)
+
+
+app.mount("#app")
